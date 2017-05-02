339938812a1f17f302fa99e57e55cc5850f91681
NataliKobriy <natalo4ka_kanjuka@mail.ru>
2017-05-02 21:42:44 +0300

    Version 1.0

---
+++ sites/all/themes/subtheme_bootstrap/js/script.js
@@ -0,0 +1,148 @@
+    // $('ul.menu.nav  li').click(function(e) {
+    //     e.preventDefault();
+    //     $('li').removeClass('active');
+    //     $(this).addClass('active');
+    // });
+    //
+
+    // @media #{$mobile} {
+    //
+    //     div.contextual-links-region.panel-pane.pane-block.pane-system-main-menu {
+    //         font-size: 1em;
+    //     }
+    //
+    //     ul.menu.nav {
+    //         position: absolute;
+    //         top: 0;
+    //         left: -16em;
+    //         width: 1em;
+    //         height: 100%;
+    //         z-index: 10;
+    //
+    //         overflow: visible;
+    //         opacity: 0.5;
+    //
+    //         box-shadow: 5px 0px 15px red;
+    //         padding-right: 1em;
+    //
+    //         transition: left 1.5s ease, opacity 1.5s ease;
+    //         -webkit-transition: left 1.5s ease, opacity 1.5s ease;
+    //     }
+    //
+    //     li a {
+    //         width: 10em;
+    //     }
+    //
+    // .active{
+    //         background: rgb(128, 156, 19);
+    //         position: relative;
+    //         color: #fff;
+    //     }
+    //
+    // .active:after {
+    //         content: '';
+    //         position: absolute;
+    //         top: 0;
+    //         left: 100%;
+    //         border: 1.1em solid transparent;
+    //         border-left: 1em solid rgb(128, 156, 19);
+    //     }
+    //
+    //     ul.menu.nav:hover {
+    //         left: 0px;
+    //         opacity: 10;
+    //
+    //         li {
+    //             background-color: white;
+    //         }
+    //         a {
+    //             width: 10em;
+    //         }
+    //
+    //     .active{
+    //             background: rgb(128, 156, 19);
+    //             position: relative;
+    //             color: #fff;
+    //         }
+    //
+    //     .active:after {
+    //             content: '';
+    //             position: absolute;
+    //             top: 0;
+    //             left: 7.5em;
+    //             border: 1em solid transparent;
+    //             border-right: 1.5em solid white;
+    //         }
+    //     }
+    // }
+    //
+    // @media #{$normal} {
+    //
+    //     div.contextual-links-region.panel-pane.pane-block.pane-system-main-menu {
+    //         font-size: 1em;
+    //     }
+    //
+    //     ul.menu.nav {
+    //         position: absolute;
+    //         top: 0;
+    //         left: -16em;
+    //         width: 1em;
+    //         height: 100%;
+    //         z-index: 10;
+    //
+    //         overflow: visible;
+    //         opacity: 0.5;
+    //
+    //         box-shadow: 5px 0px 15px red;
+    //         padding-right: 1em;
+    //
+    //         transition: left 1.5s ease, opacity 1.5s ease;
+    //         -webkit-transition: left 1.5s ease, opacity 1.5s ease;
+    //     }
+    //
+    //     li a {
+    //         width: 10em;
+    //     }
+    //
+    // .active{
+    //         background: rgb(128, 156, 19);
+    //         position: relative;
+    //         color: #fff;
+    //     }
+    //
+    // .active:after {
+    //         content: '';
+    //         position: absolute;
+    //         top: 0;
+    //         left: 100%;
+    //         border: 1.1em solid transparent;
+    //         border-left: 1em solid rgb(128, 156, 19);
+    //     }
+    //
+    //     ul.menu.nav:hover {
+    //         left: 0px;
+    //         opacity: 10;
+    //
+    //         li {
+    //             background-color: white;
+    //         }
+    //         a {
+    //             width: 10em;
+    //         }
+    //
+    //     .active{
+    //             background: rgb(128, 156, 19);
+    //             position: relative;
+    //             color: #fff;
+    //         }
+    //
+    //     .active:after {
+    //             content: '';
+    //             position: absolute;
+    //             top: 0;
+    //             left: 7.5em;
+    //             border: 1em solid transparent;
+    //             border-right: 1.5em solid white;
+    //         }
+    //     }
+    // }
